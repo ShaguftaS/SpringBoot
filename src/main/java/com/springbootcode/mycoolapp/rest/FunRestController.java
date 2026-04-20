bcad6c04b815d99fced58eaad12e66c2050c87a4
ShaguftaS <shaguftha.96@gmail.com>
2026-04-20 22:15:13 +0530

    custom application property

--- src/main/java/com/springbootcode/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/springbootcode/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,27 @@
 package com.springbootcode.mycoolapp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachname;
+
+    @Value("${team.name}")
+    private String teamname;
+
+    //expose end points for "teaminfo"
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachname + " , Team name : " + teamname ;
+    }
 
     //expose "/" that return "Hello World"
 
